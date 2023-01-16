daa023747c085b3455b73ca651f88e7864f36cda
이우승 <ss6116@daum.net>
2023-01-16 16:54:57 +0900

    BE-FEAT : 스켈레톤 코드 추가(#2)
    Former-commit-id: 367abd909ce14c80ed7edbf442a00c7e331e07a2

---
+++ backend/src/main/java/com/ssafy/db/entity/ConferenceCategory.java
@@ -0,0 +1,15 @@
+package com.ssafy.db.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class ConferenceCategory extends BaseEntity{
+
+    String name;
+
+}
