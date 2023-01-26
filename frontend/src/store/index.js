471e78a85b549b2198ed66a3908449653e5ec96a
임태민 <devtaemin@gmail.com>
2023-01-26 16:50:49 +0900

    FE-FEAT : feat-front/playroom 방생성 및 입장 기능 추가 (#12)
    Former-commit-id: ca1ab6434dc8f6e86cdf02506c27ae955ad5fd88

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,8 +5,16 @@ export default createStore({
   plugins: [createVuexPersistedState()],
 
   state: {
+    // Join Form
     myUserName: '',
-    mySessionId: ''
+    mySessionId: '',
+
+    // OpenVidu Objects
+    OV: undefined,
+    session: undefined,
+    mainStreamManager: undefined,
+    publisher: undefined,
+    subscribers: [],
   },
   getters: {
     getMyUserName: function(state) {
@@ -14,6 +22,21 @@ export default createStore({
     },
     getMySessionId: function(state) {
       return state.mySessionId
+    },
+    getOV: function(state) {
+      return state.OV;
+    },
+    getSession: function(state) {
+      return state.session;
+    },
+    getMainStreamManager: function(state) {
+      return state.mainStreamManager;
+    },
+    getPublisher: function(state) {
+      return state.publisher;
+    },
+    getSubscribers: function(state) {
+      return state.subscribers;
     }
   },
   mutations: {
@@ -28,6 +51,39 @@ export default createStore({
     },
     SET_MY_SESSION_ID: (state, payload) => {
       state.mySessionId = payload
+    },
+    INIT_OV: (state) => {
+      state.OV = undefined
+    },
+    SET_OV: (state, payload) => {
+      state.OV = payload
+    },
+    INIT_SESSION: (state) => {
+      state.session = undefined
+    },
+    SET_SESSION: (state, payload) => {
+      state.session = payload
+    },
+    INIT_MAIN_STREAM_MANAGER: (state) => {
+      state.mainStreamManager = undefined
+    },
+    SET_MAIN_STREAM_MANAGER: (state, payload) => {
+      state.mainStreamManager = payload
+    },
+    INIT_PUBLISHER: (state) => {
+      state.publisher = undefined
+    },
+    SET_PUBLISHER: (state, payload) => {
+      state.publisher = payload
+    },
+    INIT_SUBSCRIBERS: (state) => {
+      state.subscribers = []
+    },
+    SET_SUBSCRIBERS: (state, payload) => {
+      state.subscribers.push(payload)
+    },
+    DELETE_SUBSCRIBERS: (state, payload) => {
+      state.subscribers.splice(payload, 1)
     }
   },
   actions: {
@@ -43,7 +99,5 @@ export default createStore({
     setMySessionId: ({ commit }, payload) => {
       commit('SET_MY_SESSION_ID', payload)
     }
-  },
-  modules: {
   }
 })
---
+++ frontend/src/views/PlayRoomView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        플레이룸
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'PlayRoomView'
+}
+</script>
