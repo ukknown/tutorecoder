22a3311c6f64c1d0ed7cad52e40c99559e38ee3a
임태민 <devtaemin@gmail.com>
2023-01-26 13:34:28 +0900

    [MR] : 스켈레톤 코드 수정
    Former-commit-id: 40a0ee43a0f0f0976d4c24cc7e4f933129a32ade

---
+++ frontend/src/components/video/OvVideo.vue
@@ -0,0 +1,16 @@
+<template>
+    <video autoplay />
+</template>
+<script>
+export default ({
+    name: 'OvVideo',
+
+    props: {
+        streamManager: Object,
+    },
+
+    mounted() {
+        this.streamManager.addVideoElement(this.$el);
+    }
+})
+</script>
