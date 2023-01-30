6eff3ba328e82369dade5d2f5b9f7dcdb4a7891b
OctoHun <tjd_gns12@naver.com>
2023-01-30 12:01:50 +0900

    FE-FIX : solosound 컴포넌트 디자인 수정(#19)
    Former-commit-id: 84a8a51735598aa482586b22b0b468dbfac60af6

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,6 +5,13 @@
 </template>
 
 <style>
+
+@font-face { /* JUA 폰트 */
+  font-family: "JUA";
+  src: url("./assets/fonts/Jua-Regular.ttf") format("truetype");
+  font-weight: normal;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -12,6 +19,7 @@
   text-align: center;
   color: #2c3e50;
   height: 100vh;
+  font-family: 'JUA', serif;
 }
 
 #background-image{
@@ -19,5 +27,7 @@
   height: 100vh;
   background-size: 100vw 100vh;
 }
+
+
 
 </style>
