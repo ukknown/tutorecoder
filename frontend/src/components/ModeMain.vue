4b4d80b6f4e6ed231210b220ba0ae35ddba2eaf3
OctoHun <tjd_gns12@naver.com>
2023-01-30 17:12:54 +0900

    FE-FEAT : 혼자하기-분석표 작업(#20)
    Former-commit-id: b8ad5cd4a48e64a48d0e7ec8989912354fca85a5

--- frontend/src/components/ModeMain.vue
+++ frontend/src/components/ModeMain.vue
@@ -24,6 +24,7 @@
             v-model="roomEnterVisible"
             title=""
             width="40%"
+            class="tutorial-modal"
         >
             <el-form
                 label-width="100px"
@@ -113,3 +114,9 @@ export default {
     }
 }
 </script>
+
+<style>
+.tutorial-modal{
+    background-color: #F2E6E6;;
+}
+</style>
