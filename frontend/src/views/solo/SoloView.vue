441e58a68a2c1c58a102bbc54c40964f1ed9ae6f
njh3321 <njh3321@naver.com>
2023-01-31 16:59:21 +0900

    EXTRA : Conflict 해소(#21)
    Former-commit-id: b8b227149a9a983129bba11e08266afa481bb616

--- frontend/src/views/solo/SoloView.vue
+++ frontend/src/views/solo/SoloView.vue
@@ -3,47 +3,14 @@
         <SoloMain/>
     </div>
 </template>
+
 <script>
-<<<<<<< HEAD
-import SoloMain from '@/components/solo/SoloMain.vue';
-=======
 import SoloMain from '@/components/solo/SoloMain.vue'
->>>>>>> f9ae546332c2dfbc3fdd2bc6e06a22e89800cc59
 
 export default {
     name: 'SoloView',
     components: {
         SoloMain,
-<<<<<<< HEAD
-    },
-
-}
-</script>
-
-<style>
-
-.solo-analyze-button{
-    background-color: #8684FF;
-    color: white;
-    font-family: 'JUA', serif;
-    font-size: 4vh;
-    width: 18vh;
-    height: 6vh;
-    margin-right: 8vh;
-
-}
-
-.solo-out-button{
-    background-color: #F0AFAF;
-    font-family: 'JUA', serif;
-    font-size: 4vh;
-    width: 18vh;
-    height: 6vh;
-}
-</style>
-=======
     }
-
 }
 </script>
->>>>>>> f9ae546332c2dfbc3fdd2bc6e06a22e89800cc59
