ec17387bcf4953e77c6dcb9131e50b41d3eef4c3
OctoHun <tjd_gns12@naver.com>
2023-02-01 09:27:14 +0900

    FE-FEAT : multisong 디자인 수정(#25)
    Former-commit-id: 068214390c5794e7534c565f2b7b8454d40e4913

--- frontend/src/views/multi/MultiSongView.vue
+++ frontend/src/views/multi/MultiSongView.vue
@@ -17,4 +17,22 @@ export default {
 
 <style>
 
+.solo-out-button{
+    background-color: #F0AFAF;
+    font-family: 'JUA', serif;
+    font-size: 4vh;
+    width: 10vw;
+    height: 6vh;
+}
+
+.solo-analyze-button{
+    background-color: #8684FF;
+    color: white;
+    font-family: 'JUA', serif;
+    font-size: 4vh;
+    width: 10vw;
+    height: 6vh;
+    margin-right: 3vw;
+}
+
 </style>
