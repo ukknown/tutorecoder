2ea7e602a28b175fae892e14da116c93965e8d0c
OctoHun <tjd_gns12@naver.com>
2023-02-02 09:41:19 +0900

    FE-FEAT : 기능만 추가(#31)
    Former-commit-id: e14055aef683f74007694ae873ffdfc3b8e87b12

--- frontend/src/components/solo/SoloSoundMain.vue
+++ frontend/src/components/solo/SoloSoundMain.vue
@@ -7,16 +7,29 @@
             <div class="game-content-title">소리내기</div>
             <div class="game-content-target">
                 <img id="board-image" src="../../assets/board.png" alt="">
+                <span>{{ pitch_target }}</span>
             </div>
             <div class="game-content-button">
                 <el-button class="solo-analyze-button" @click="goSoloAnalize">분석</el-button>
                 <el-button class="solo-out-button" @click="goSolo">나가기</el-button>
+                <button type="button" @click="init">Start</button>
+                <div id="label-container"></div>
             </div>
         </el-col>
     </el-row>
 </template>
 
 <script>
+import '@tensorflow/tfjs'
+import * as speechCommands from '@tensorflow-models/speech-commands'
+
+const pitch_list = ['도', '레', '미', '파', '솔', '라', '시'];
+let pick_list = []
+for (let i=0; i<5; i++) {
+    pick_list.push(pitch_list[Math.floor(Math.random() * 7)])
+}
+
+
 export default {
     name: 'SoloSoundMain',
     props: {
@@ -28,9 +41,57 @@ export default {
         },
         goSolo() {
             this.$router.push({ name: 'solo' })
+        },
+        async createModel () {
+      const URL = 'https://teachablemachine.withgoogle.com/models/m6ms0qdPf/'
+
+      const checkpointURL = URL + 'model.json' // model topology
+      const metadataURL = URL + 'metadata.json' // model metadata
+
+      const recognizer = speechCommands.create(
+        'BROWSER_FFT', // fourier transform type, not useful to change
+        undefined, // speech commands vocabulary feature, not useful for your models
+        checkpointURL,
+        metadataURL)
+
+      // check that model and metadata are loaded via HTTPS requests.
+      await recognizer.ensureModelLoaded()
+
+      return recognizer
+    },
+    async init () {
+      const recognizer = await this.createModel() // 모델 생성
+      const classLabels = recognizer.wordLabels() // get class labels, 학습 시킨 클래스들
+      const labelContainer = document.getElementById('label-container') // 데이터 라벨 생성
+      for (let i = 0; i < classLabels.length; i++) {
+        labelContainer.appendChild(document.createElement('div'))
+      }
+
+      // listen() takes two arguments:
+      // 1. A callback function that is invoked anytime a word is recognized.
+      // 2. A configuration object with adjustable fields
+      recognizer.listen(result => {
+        const scores = result.scores // eslint-disable-line no-unused-vars
+        // render the probability scores per class
+        for (let i = 0; i < classLabels.length; i++) {
+          const classPrediction = classLabels[i] + ': ' + result.scores[i].toFixed(2) // 소숫점까지 표기(2자리)
+        //   console.log('음계' + classLabels[i])
+        //   console.log('점수' + result.scores[i])
+          labelContainer.childNodes[i].innerHTML = classPrediction
         }
+      }, {
+        includeSpectrogram: true, // in case listen should return result.spectrogram
+        probabilityThreshold: 0.75,
+        invokeCallbackOnNoiseAndUnknown: true,
+        overlapFactor: 0.50 // probably want between 0.5 and 0.75. More info in README
+      })
+
+      // Stop the recognition in 5 seconds.
+      // setTimeout(() => recognizer.stopListening(), 5000);
     }
+  }
 }
+
 </script>
 
 <style>
