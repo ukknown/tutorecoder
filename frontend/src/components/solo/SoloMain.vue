e86fbf30a83d48300a46a9f85439b25ad3c60d18
kim hyeon-seo_h <munhwa95@naver.com>
2023-02-05 18:12:18 +0900

    FIX-FRONT : 이미지 수정,  확인 필요
    Former-commit-id: 1a8f5bc34bbaa9f70ab20b3e727f0dbd7229542c

--- frontend/src/components/solo/SoloMain.vue
+++ frontend/src/components/solo/SoloMain.vue
@@ -4,34 +4,34 @@
             <img id='logo' src="@/assets/logo_nocircle.png" alt="logo img" >
         </div>
 
-        <div id="containerStack">
-            <div id="cardContainer" @click="moveSoloTutorial">
-                <!-- <div><img id="iconImg" src="@/assets/soloSelect-tutorial.png" alt="튜토리얼 img"></div> -->
-                <div><img id="iconImg" src="@/assets/rectan.png" alt="튜토리얼 img"></div>
-                <div><img id="textImg" src="@/assets/text/solo_tutorial.png" alt="튜토리얼"></div>
+        <div id="blueBoxContainer">
+            <div id="redBoxComponent" @click="moveSoloTutorial">
+                <img id="iconImg" src="@/assets/soloSelect-tutorial.png" alt="튜토리얼 img">
+                <!-- <img id="iconImg" src="@/assets/rectan.png" alt="튜토리얼 img"> -->
+                <img id="textImg" src="@/assets/text/solo_tutorial.png" alt="튜토리얼">
+
             </div>
-            <div id="cardContainer" @click="moveSoloSound">
-                <!-- <div><img id="iconImg" src="@/assets/soloSelect-sound.png" alt="소리내기 img"></div> -->
-                <div><img id="iconImg" src="@/assets/rectan.png" alt="소리내기 img"></div>
-                <div><img id="textImg" src="@/assets/text/solo_sound.png" alt="소리내기"></div>
+            <div id="redBoxComponent" @click="moveSoloSound">
+                <img id="iconImg" src="@/assets/soloSelect-sound.png" alt="소리내기 img">
+                <!-- <img id="iconImg" src="@/assets/rectan.png" alt="소리내기 img"> -->
+                <img id="textImg" src="@/assets/text/solo_sound.png" alt="소리내기">
             </div>
-            <div id="cardContainer" @click="moveSoloSong">
-                <!-- <div><img id="iconImg" src="@/assets/soloSelect-song.png" alt="연주하기 img"></div> -->
-                <div><img id="iconImg" src="@/assets/rectan.png" alt="연주하기 img"></div>
-                <div><img id="textImg" src="@/assets/text/solo_song.png" alt="연주하기"></div>
+            <div id="redBoxComponent" @click="moveSoloSong">
+                <img id="iconImg" src="@/assets/soloSelect-song.png" alt="연주하기 img">
+                <!-- <img id="iconImg" src="@/assets/rectan.png" alt="연주하기 img">-->
+                <img id="textImg" src="@/assets/text/solo_song.png" alt="연주하기">
             </div>
         </div>
 
         
         <!-- 뒤로가기, 홈버튼 -->
         <!-- 이미지 방향은 놔두기 playroom branch 에서 수정함 -->
-        <!-- <div id="iconContainer">
-            <div @click="goback">
-                <img id="backImg" src="@/assets/goback.png" alt="돌아가기">
-                <span>돌아가기</span>
-            </div> -->
-            <!-- <img id="homeImg" src="@/assets/homeButton.png" alt="홈" @click="goHome"> -->
-        <!-- </div> -->
+        <div id="iconContainer">
+            <span @click="goback" id="gobackIcon">
+                <img id="gobackImg" src="@/assets/goback.png" alt="돌아가기">
+            </span>            
+            <img id="homeImg" src="@/assets/homeButton.png" alt="홈" @click="goHome">
+        </div>
         <!-- style="cursor: pointer; width: 5%; position: absolute; left: 48%; bottom: 5%;" -->
         <!-- 뒤로가기, 홈버튼  -->
         
@@ -80,80 +80,74 @@ export default {
     justify-items: center;
     align-items: center;
     margin-top: 5vh;
-    position: relative;
 }
-
-/* #logoContainter{
-    border: 5px solid yellow;
-    width: 50%;
-    margin-top: 2%;
-    position: inherit;
-} */
+#logoContainter{
+    width: 40%;
+}
 
 #logo{
     border: 5px solid yellow;
-    position: absolute;
-    width: 25%;
-    height: 20%;
+    width: 60%;
     margin-top: 2%;
-        
 }
 
 /* 카드 */
-#containerStack{
-    border: 10px solid blue;
-    width: 70%;
-    height: 40%;
-    margin-top: 13%;
-    position: absolute;
-}
-#cardContainer{
-    position: absolute;
-    border: 5px solid rgb(141, 46, 67);
-    width: 100%;
-    height: 100%;
+#blueBoxContainer{
+    border: 5px solid blue;
     display: flex;
-    justify-items: left;
+    width: 80%;
+    height: 60%;
+}
+#redBoxComponent{
+    border: 5px solid red;
+    width: 40%;
+    height: 99%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    background-color: #F2E6E6;   
+    border-radius: 20px;
+    margin-left: 2%;
+    margin-right: 2%;
+    
 }
 
 #iconImg{
-    border: 5px solid red;
-    width: 50%;
-    height: 80%;
-    position: absolute;
+    border: 5px solid violet;
+    /* height: 60%; */
+    width: 60%;
 } 
 #textImg{
-    border: 10px solid greenyellow;
-    width: 50%;
-    height: 50%;
-    position: absolute;
-    left: 25%;
+    border: 5px solid gold;
+    width: 40%;
+    margin-top: 10%;
 }
+
 .el-card{
-    border: 5px solid red;
-    cursor: pointer;
-    border-radius: 20px;
-    background-color: #F2E6E6;
-    position: absolute;
-    height: 100%;
-    
 }
 /* 카드 끝 */
 
 
 /* 하단 아이콘 */
 #iconContainer{
-    width:90%;
-    height:10%;
+    border: 5px solid  white;
+    width: 99%;
+    margin-top: 2%;
     display: flex;
-    border: 5px solid pink;
-    font-size: 4em;    
+}
+#gobackIcon{
+    width: 21%;
+}
+#gobackImg{
+    width: 100%;
+    border: 2px solid greenyellow;
+    margin-right:2%;
 }
 #homeImg{
-    width:5%;
-}
-#backImg{
-    width:5%;
+    width: 5%;
+    border: 2px solid blue;
 }
 /* 하단 아이콘 끝 */
 
