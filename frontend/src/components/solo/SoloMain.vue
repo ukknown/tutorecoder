57dd812d8a643cc2ee14f2bcce0bfd31e5b78651
pjt laptop <munhwa3278@gmail.com>
2023-02-06 11:26:19 +0900

    FE-FIX : 모드 선택 화면 디자인 확정 (#37)
    Former-commit-id: 0f25df5e2c05d8edde6b213bb5c3d2bb3fb6b014

--- frontend/src/components/solo/SoloMain.vue
+++ frontend/src/components/solo/SoloMain.vue
@@ -7,18 +7,15 @@
         <div id="blueBoxContainer">
             <div id="redBoxComponent" @click="moveSoloTutorial">
                 <img id="iconImg" src="@/assets/soloSelect-tutorial.png" alt="튜토리얼 img">
-                <!-- <img id="iconImg" src="@/assets/rectan.png" alt="튜토리얼 img"> -->
                 <img id="textImg" src="@/assets/text/solo_tutorial.png" alt="튜토리얼">
 
             </div>
             <div id="redBoxComponent" @click="moveSoloSound">
                 <img id="iconImg" src="@/assets/soloSelect-sound.png" alt="소리내기 img">
-                <!-- <img id="iconImg" src="@/assets/rectan.png" alt="소리내기 img"> -->
                 <img id="textImg" src="@/assets/text/solo_sound.png" alt="소리내기">
             </div>
             <div id="redBoxComponent" @click="moveSoloSong">
                 <img id="iconImg" src="@/assets/soloSelect-song.png" alt="연주하기 img">
-                <!-- <img id="iconImg" src="@/assets/rectan.png" alt="연주하기 img">-->
                 <img id="textImg" src="@/assets/text/solo_song.png" alt="연주하기">
             </div>
         </div>
@@ -36,7 +33,6 @@
                 <img id="homeText" src="@/assets/homeText.png" alt="홈">
             </span>            
         </div>
-        <!-- style="cursor: pointer; width: 5%; position: absolute; left: 48%; bottom: 5%;" -->
         <!-- 뒤로가기, 홈버튼  -->
         
     </div>
@@ -70,96 +66,95 @@ export default {
 </script>
 
 <style scoped>
-.hover-button{
-    cursor: pointer;
-}
+    #backgroundContainer{
+        border-radius: 30px;
+        width: 90vw;  
+        height: 90vh;
+        background-color: rgba(0, 0, 0, 0.374);
+        display: flex;
+        flex-direction: column;
+        justify-items: center;
+        align-items: center;
+        margin-top: 5vh;
+    }
+    #logoContainter{
+        width: 40%;
+    }
 
-#backgroundContainer{
-    border-radius: 30px;
-    width: 90vw;  
-    height: 90vh;
-    background-color: rgba(0, 0, 0, 0.374);
-    display: flex;
-    flex-direction: column;
-    justify-items: center;
-    align-items: center;
-    margin-top: 5vh;
-}
-#logoContainter{
-    width: 40%;
-}
+    #logo{
+        /* border: 5px solid yellow; */
+        width: 60%;
+        margin-top: 2%;
+    }
 
-#logo{
-    border: 5px solid yellow;
-    width: 60%;
-    margin-top: 2%;
-}
+    /* 카드 */
+    #blueBoxContainer{
+        /* border: 5px solid blue; */
+        display: flex;
+        width: 80%;
+        height: 60%;
+    }
+    #redBoxComponent{
+        /* border: 5px solid red; */
+        width: 40%;
+        height: 99%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
 
-/* 카드 */
-#blueBoxContainer{
-    border: 5px solid blue;
-    display: flex;
-    width: 80%;
-    height: 60%;
-}
-#redBoxComponent{
-    border: 5px solid red;
-    width: 40%;
-    height: 99%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+        background-color: #F2E6E6;   
+        border-radius: 20px;
+        margin-left: 2%;
+        margin-right: 2%;
+        
+    }
 
-    background-color: #F2E6E6;   
-    border-radius: 20px;
-    margin-left: 2%;
-    margin-right: 2%;
-    
-}
-
-#iconImg{
-    border: 5px solid violet;
-    width: 60%;
-} 
-#textImg{
-    border: 5px solid gold;
-    width: 60%;
-    margin-top: 10%;
-}
+    #iconImg{
+        /* border: 5px solid violet; */
+        width: 60%;
+    } 
+    #textImg{
+        /* border: 5px solid gold; */
+        width: 60%;
+        margin-top: 10%;
+    }
 
 
-/* 카드 끝 */
+    /* 카드 끝 */
 
 
-/* 하단 아이콘 */
-#iconContainer{
-    border: 5px solid  white;
-    width: 99%;
-    margin-top: 2%;
-    display: flex;
-    justify-content: space-between;
-}
-#subIconContainer{
-    border: 5px solid black;
-    width: 10%;
-}
-#gobackImg{
-    width: 40%;
-    border: 2px solid greenyellow;
-}
-#gobackText{
-    width: 50%;
-    border: 2px solid violet;
-}
-#homeImg{
-    width: 40%;
-    border: 2px solid blue;
-}
-#homeText{
-    width: 50%;
-    border: 2px solid red;
-}
-/* 하단 아이콘 끝 */
+    /* 하단 아이콘 */
+    #iconContainer{
+        /* border: 5px solid  white; */
+        width: 99%;
+        margin-top: 2%;
+        display: flex;
+        justify-content: space-between;
+        position: absolute;
+        bottom: 5vh;
+    }
+    #subIconContainer{
+        /* border: 5px solid black; */
+        width: 10%;
+        min-width: 15vh;
+    }
+    #gobackImg{
+        width: 40%;
+        /* border: 2px solid greenyellow; */
+    }
+    #gobackText{
+        width: 50%;
+        /* border: 2px solid violet; */
+    }
+    #homeImg{
+        width: 40%;
+        /* border: 2px solid blue; */
+    }
+    #homeText{
+        width: 50%;
+        /* border: 2px solid red; */
+    }
+    /* 하단 아이콘 끝 */
 
 </style>
