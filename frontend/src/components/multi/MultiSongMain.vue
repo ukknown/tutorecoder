2325e1d5b9d71133cc536af4cf1a4bd58fc26a1a
njh3321 <njh3321@naver.com>
2023-02-06 10:50:35 +0900

    FE-FEAT : mutli-song에 Carousel 추가(#36)
    Former-commit-id: 486893a352f60ef3937074dd512549fdf986da4c

--- frontend/src/components/multi/MultiSongMain.vue
+++ frontend/src/components/multi/MultiSongMain.vue
@@ -6,8 +6,19 @@
             </el-col>
         </el-row>
         <el-row class="game-sub-container">
-            <el-col :span="15" class="game-sub-my-cam">
-                내 카메라
+            <el-col :span="15" class="game-sub-cam-section">
+                <el-col :span="6" class="game-content-others-cam">
+                    <el-button class="carousel-button" :icon="ArrowUpBold" @click="goPrev"></el-button>
+                    <div class="carousel" id="cam-carousel">
+                        <div v-for="other in others" :key="other" class="caroursel-item">
+                            <h3>{{ other }}</h3>
+                        </div>
+                    </div>
+                    <el-button class="carousel-button" :icon="ArrowDownBold" @click="goNext"></el-button>
+                </el-col>
+                <el-col :span="18" class="game-content-my-cam">
+                    내 카메라 들어가는 곳
+                </el-col>
             </el-col>
             <el-col :span="8" :offset="1" class="game-sub-info">
                 <div class="game-sub-title">곡 제목</div>
@@ -23,20 +34,37 @@
     </div>
 </template>
 <script>
-    export default {
-        name: 'MultiSongMain',
-        props: {
-            msg: String
+import { ArrowUpBold, ArrowDownBold } from '@element-plus/icons-vue'
+
+export default {
+    name: 'MultiSongMain',
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+            ArrowUpBold: ArrowUpBold,
+            ArrowDownBold: ArrowDownBold,
+            others: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+        }
+    },
+    methods: {
+        // goSoloAnalize() {
+        //     this.$router.push({ name: 'soloAnalize' })
+        // },
+        // goSolo() {
+        //     this.$router.push({ name: 'solo' })
+        // },
+        goNext() {
+            let height = document.getElementById('cam-carousel').clientHeight;
+            document.getElementById('cam-carousel').scrollTop += height; 
         },
-        methods: {
-            goSoloAnalize() {
-                this.$router.push({ name: 'soloAnalize' })
-            },
-            goSolo() {
-                this.$router.push({ name: 'solo' })
-            }
+        goPrev() {
+            let height = document.getElementById('cam-carousel').clientHeight;
+            document.getElementById('cam-carousel').scrollTop -= height; 
         }
     }
+}
 </script>
 <style>
 .container{
@@ -60,10 +88,11 @@
     width : 95%;
 }
 
-.game-sub-my-cam{
+.game-sub-cam-section{
     height: 100%;
-    border: 1px solid blue;
     border-radius: 20px;
+    background-color: rgba(0, 0, 0, 0.374);
+    height: 100%;
 }
 
 .game-sub-info{
@@ -87,5 +116,46 @@
 
 .game-sub-button{
     margin-top: 5vh;
+}
+.game-content-others-cam{
+    margin: auto;
+    height: 100%;
+    border: 1px solid orange;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.game-content-my-cam {
+    margin: auto;
+    height: 100%;
+    border: 1px solid orange;
+    border-radius: 20px;
+}
+.carousel-button {
+    width: 3vw;
+    background-color: #F0CDCD;
+    color: #6A6D71;
+    margin-top: 1vh;
+    margin-bottom: 1vh;
+    cursor: url(../../assets/cursor_click.png), auto !important;
+}
+#cam-carousel{
+    width: 100%;
+    height: 87%;
+    overflow-y: auto;
+    scroll-behavior: smooth;
+}
+#cam-carousel::-webkit-scrollbar{
+    display:none;
+}
+.caroursel-item {
+    height: 24.7%;
+    width: 99.5%;
+    background-color: #9fa1a4;
+    overflow: hidden;
+    border: 1px solid black;
+    border-radius: 10px;
 }
 </style>
