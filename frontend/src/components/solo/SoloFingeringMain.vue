66802abc34b955e671e5cbe32f65ed082dac2691
jongukchoi95 <jongukchoi95@gmail.com>
2023-02-09 15:03:07 +0900

    FE-FEAT : 운지법 구분 기능 추가(#51)
    Former-commit-id: 31003db1b2cbe856694b3f51ebfeed4a39c6e506

--- frontend/src/components/solo/SoloFingeringMain.vue
+++ frontend/src/components/solo/SoloFingeringMain.vue
@@ -1,14 +1,11 @@
 <template>
     <el-row class="game-content-container">
-        <!-- <el-col :span="15" class="game-content-my-cam">
-            <user-video :stream-manager="mainStreamManager"/>
-        </el-col> -->
         <el-col :span="15" class="game-content-my-cam">
         <div id="webcam-container"></div>
         <div id="label-container"></div>
         </el-col>
         <el-col :span="8" class="game-content-info">
-            <div class="game-content-title">소리내기</div>
+            <div class="game-content-title">운지법</div>
             <div class="game-content-target">
                 <img id="board-image" src="../../assets/board.png">
                 <span class="pitch-target" style="margin-top:">{{ pitch_target }}</span>
@@ -33,7 +30,7 @@ import '@tensorflow/tfjs'
 import { OpenVidu } from "openvidu-browser";
 import axios from "axios";
 //import UserVideo from "@/components/video/soloUserVideo.vue"
-import * as speechCommands from '@tensorflow-models/speech-commands'
+
 import { mapActions } from 'vuex'
 import * as tmImage from '@teachablemachine/image'
 
@@ -59,7 +56,6 @@ const problem = 3
 
 // pick_list.push('참 잘했어요')
 const total_problem = problem + 9;
-const URL = "https://teachablemachine.withgoogle.com/models/eptQYA8MT/";
 
 let model, webcam, labelContainer, maxPredictions;
 const ImageURL = "https://teachablemachine.withgoogle.com/models/7V-1Hlw2G/"; //image 데이터
@@ -122,112 +118,6 @@ export default {
             this.$router.push({ name: 'solo' })
         },
         
-        async createModel () {
-
-            
-            const checkpointURL = URL + 'model.json' // model topology
-            const metadataURL = URL + 'metadata.json' // model metadata
-
-            const recognizer = speechCommands.create(
-                'BROWSER_FFT', // fourier transform type, not useful to change
-                undefined, // speech commands vocabulary feature, not useful for your models
-                checkpointURL,
-                metadataURL
-            )
-
-            // check that model and metadata are loaded via HTTPS requests.
-            await recognizer.ensureModelLoaded()
-
-            return recognizer
-        },
-        async init () {
-            const recognizer = await this.createModel() // 모델 생성
-            const classLabels = recognizer.wordLabels() // get class labels, 학습 시킨 클래스들
-            // 실시간으로 점수 표시해주는 역할(필요없어서 지움)
-            // const labelContainer = document.getElementById('label-container') // 데이터 라벨 생성
-            // for (let i = 0; i < classLabels.length; i++) {
-            //     labelContainer.appendChild(document.createElement('div'))
-            // }
-
-            // listen() takes two arguments:
-            // 1. A callback function that is invoked anytime a word is recognized.
-            // 2. A configuration object with adjustable fields
-            recognizer.listen(result => {
-                const scores = result.scores // eslint-disable-line no-unused-vars
-                // render the probability scores per class
-                for (let i = 0; i < classLabels.length; i++) {
-                    // const classPrediction = classLabels[i] + ': ' + result.scores[i].toFixed(2) // 소숫점까지 표기(2자리)
-                    //   console.log('음계' + classLabels[i])
-                    //   console.log('점수' + result.scores[i])
-                    // 도, 레, 미, 파, 솔, 라, 시, 음이탈, 바람빠지는소리, 배경소음
-                    const index = result.scores.indexOf(Math.max(...result.scores));
-                    switch(this.pitch_target) {
-                        case '도':
-                            if (index === 0) {
-                                grade_list[0].push(result.scores[index])
-                            } else if (index !== 9){
-                                grade_list[0].push(0)
-                            }
-                            break;
-                        case '레':
-                            if (index === 1) {
-                                grade_list[1].push(result.scores[index])
-                            } else if (index !== 9){
-                                grade_list[1].push(0)
-                            }
-                            break;
-                        case '미':
-                            if (index === 2) {
-                                grade_list[2].push(result.scores[index])
-                            } else if (index !== 9){
-                                grade_list[2].push(0)
-                            }
-                            break;
-                        case '파':
-                            if (index === 3) {
-                                grade_list[3].push(result.scores[index])
-                            } else if (index !== 9){
-                                grade_list[3].push(0)
-                            }
-                            break;
-                        case '솔':
-                            if (index === 4) {
-                                grade_list[4].push(result.scores[index])
-                            } else if (index !== 9){
-                                grade_list[4].push(0)
-                            }
-                            break;
-                        case '라':
-                            if (index === 5) {
-                                grade_list[5].push(result.scores[index])
-                            } else if (index !== 9){
-                                grade_list[5].push(0)
-                            }
-                            break;
-                        case '시':
-                            if (index === 6) {
-                                grade_list[6].push(result.scores[index])
-                            } else if (index !== 9){
-                                grade_list[6].push(0)
-                            }
-                            break;
-                        default:
-                            // code block for default case
-                    }
-
-                    // 실시간 점수 표시해주는 역할(필요없어서 지움)
-                    // labelContainer.childNodes[i].innerHTML = classPrediction
-                }
-            }, {
-                includeSpectrogram: true, // in case listen should return result.spectrogram
-                probabilityThreshold: 0.75,
-                invokeCallbackOnNoiseAndUnknown: true,
-                overlapFactor: 0.50 // probably want between 0.5 and 0.75. More info in README
-            })
-
-            // Stop the recognition in 5 seconds.
-            // setTimeout(() => recognizer.stopListening(), 5000);
-        },
         gameStart() {
             this.initGameResult()
             pitch_list = ['도', '레', '미', '파', '솔', '라', '시'];
