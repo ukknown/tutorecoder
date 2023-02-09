ddd3934c59bbeabbac50c8d539e75a7d73c3cc3f
OctoHun <tjd_gns12@naver.com>
2023-02-09 15:43:26 +0900

    FE-FEAT : 레디여부 표시(#49)
    Former-commit-id: 2fc23c4766c10371512b6b1edc9457c8d67abfcc

--- frontend/src/components/video/UserVideo.vue
+++ frontend/src/components/video/UserVideo.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="streamManager" id="video-div">
-        <ov-video :stream-manager="streamManager"  id="video-box"/>
+        <ov-video :stream-manager="streamManager" id="video-box"/>
         <div style="margin:0; padding:0;"> {{ clientData }} </div>
     </div>
 </template>
@@ -34,8 +34,8 @@ export default {
 </script>
 
 <style scoped>
+
   #video-div{
-    border: 5px solid blue ;
     border-radius: 10px;
     display: inline-block;
     margin: 2%;
