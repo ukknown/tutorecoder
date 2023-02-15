d3f9afffa9b7be20c1c9d0410e45830f08cc2dbd
OctoHun <tjd_gns12@naver.com>
2023-02-15 13:07:12 +0900

    EXTRA : DEVELOP Recovery
    Former-commit-id: 694ac1efbe6e2ca7eef644a7c17f538c7001d4bc

---
+++ frontend/src/views/solo/SoloView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mode">
+        <SoloMain/>
+    </div>
+</template>
+
+<script>
+import SoloMain from '@/components/solo/SoloMain.vue'
+
+export default {
+    name: 'SoloView',
+    components: {
+        SoloMain,
+    }
+}
+</script>
+
+<style>
+</style>
